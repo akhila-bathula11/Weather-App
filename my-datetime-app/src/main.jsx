49306c5faf53b0aec6b714607f45f1ce19bbf254
Akhila <akhilabathula11@gmail.com>
2026-06-13 01:26:33 +0530

    Update gitignore

--- my-datetime-app/src/main.jsx
+++ my-datetime-app/src/main.jsx
@@ -1,9 +1,11 @@
+
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-import {Flipkart} from "./components/Flipkart/flipkart.jsx"
 
-createRoot(document.getElementById("root")).render(<Flipkart/>);
+import { WeatherApp } from "./components/weather-app/weather-app.jsx";
+
+createRoot(document.getElementById("root")).render(<WeatherApp/>);
